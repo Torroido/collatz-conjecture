c5d5c015636815e4050d129df682264bcb8d2de9
Parth Patki <parthpatki2002@gmail.com>
2025-06-27 12:00:00 +0530

    Added plot method

--- lib/collatz_conjecture.rb
+++ lib/collatz_conjecture.rb
@@ -16,5 +16,24 @@ module CollatzConjecture
       end
       sequence
     end
+
+    def self.plot(n, height: 20, symbol: "*")
+      sequence = solve(n)
+      max_value = sequence.max
+      scaled = sequence.map { |v| (v.to_f / max_value * (height - 1)).round }
+
+      grid = Array.new(height) { Array.new(sequence.size, " ") }
+      scaled.each_with_index do |y, x|
+        grid[height - 1 - y][x] = symbol
+      end
+
+      grid.each_with_index do |row, idx|
+        y_label = ((height - 1 - idx) * max_value / (height - 1)).round.to_s.rjust(4)
+        puts "#{y_label} | #{row.join}"
+      end
+
+      puts "     +" + "-" * sequence.size
+      puts "      " + (0...sequence.size).map { |i| (i % 10).to_s }.join
+    end
   end
 end
